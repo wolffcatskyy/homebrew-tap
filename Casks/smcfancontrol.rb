0a982d487d883a56cf7712264950cfc630faa6db
wolffcatskyy <153394885+wolffcatskyy@users.noreply.github.com>
2026-03-18 10:44:20 -0400

    Update smcfancontrol cask to v2.7.0

--- Casks/smcfancontrol.rb
+++ Casks/smcfancontrol.rb
@@ -1,6 +1,6 @@
 cask "smcfancontrol" do
-  version "2.6.2"
-  sha256 "f167e0950ddee16c6780b51bc92b06159c89fae8c85a5249dbccded7b36ab9a8"
+  version "2.7.0"
+  sha256 "4feef95081bc8b469ff6e1609cb48f5d1df4342ab542edeb2d6328b88f644d82"
 
   url "https://github.com/wolffcatskyy/smcFanControl/releases/download/v#{version}-ce/smcFanControl_CE_#{version}.zip"
   name "smcFanControl Community Edition"
